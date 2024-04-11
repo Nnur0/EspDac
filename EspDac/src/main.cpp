9f235cf7dbc494190727d39159136a2364375c2c
Nnur0 <117279667+Nnur0@users.noreply.github.com>
2024-04-11 16:01:54 +0200

    I2S.h in test

--- EspDac/src/main.cpp
+++ EspDac/src/main.cpp
@@ -1,14 +1,15 @@
+
 #include <Arduino.h>
 #include <I2S.h>
 
 
 const long sampleRate = 48000;
-const int bitsPerSample = 32;
+const int bitsPerSample = 16;
 uint8_t *buffer;
 
 
 int y = 0;
-int y1 = 0;
+//int y1 = 0;
 int y2 = 0;
 int y3 = 0;
 int y4 = 0;
@@ -20,79 +21,85 @@ int x3[5] = {0};
 int x4[5] = {0};
 
 enum mode {
-  RH,
-  LH,
-  RM,
-  LM,
+  H,
+  M,
   L
 };
 
-int filter(mode mode){
+int filter(mode _mode){
   double a[4];
   double b[5];
 
-  switch ( mode )
+  if (_mode == H)
   {
-    case mode::RH:
-    case mode::LH:
-      double _a[4] = { 4.00007043, -6.0002113, 4.0002113, -1.00007043};
-      *a = *_a;
-      double _b[5] = { 0.03543882, -0.14175529,  0.21263294, -0.14175529,  0.03543882};
-      *b = *_b;
-    break;
+    double Ha[4] = { 4.00007043, -6.0002113, 4.0002113, -1.00007043};
+    *a = *Ha;
+    double Hb[5] = { 0.03543882, -0.14175529,  0.21263294, -0.14175529,  0.03543882};
+    *b = *Hb;
 
-    case mode::RM:
-    case mode::LM:
-      double _a[4] = { 4.00007043, -6.0002113, 4.0002113, -1.00007043};
-      *a = *_a;
-      double _b[5] = { 0.03543882, -0.14175529,  0.21263294, -0.14175529,  0.03543882};
-      *b = *_b;
-    break;
-
-    case mode::L:
-      double _a[4] = { 4.00007043, -6.0002113, 4.0002113, -1.00007043};
-      *a = *_a;
-      double _b[5] = { 0.03543882, -0.14175529,  0.21263294, -0.14175529,  0.03543882};
-      *b = *_b;
-    break;    
-
-    default:
-    break;
   }
+  else if (_mode == M)
+  {
+    double Ma[4] = { 4.00007043, -6.0002113, 4.0002113, -1.00007043};
+    *a = *Ma;
+    double Mb[5] = { 0.03543882, -0.14175529,  0.21263294, -0.14175529,  0.03543882};
+    *b = *Mb;
+  }
+    else if (_mode == L)
+  {
+    double La[4] = { 4.00007043, -6.0002113, 4.0002113, -1.00007043};
+    *a = *La;
+    double Lb[5] = { 0.03543882, -0.14175529,  0.21263294, -0.14175529,  0.03543882};
+    *b = *Lb;
+  }
+
+  return 0;
 }
+
 
 void setup() {
   Serial.begin(115200);
-  //I2S.setAllPins(5, 25, 35, 26); // you can change default pins; order of pins = (CLK, WS, IN, OUT)
-  if(!I2S.setDuplex()){
-    Serial.println("ERROR - could not set duplex");
-    while(true){
-      vTaskDelay(10); // Cannot continue
-    }
-  }
-  if (!I2S.begin(I2S_PHILIPS_MODE, sampleRate, bitsPerSample)) {
+
+    if (!I2S.begin(I2S_PHILIPS_MODE, 48000, 16)) {
     Serial.println("Failed to initialize I2S!");
-    while(true){
-      vTaskDelay(10); // Cannot continue
-    }
+    while (1); // do nothing
   }
-  buffer = (uint8_t*) malloc(I2S.getBufferSize() * (bitsPerSample / 8));
-  if(buffer == NULL){
-    Serial.println("Failed to allocate buffer!");
-    while(true){
-      vTaskDelay(10); // Cannot continue
-    }
-  }
-  Serial.println("Setup done");
+  //I2S.setAllPins(5, 25, 35, 26); // you can change default pins; order of pins = (CLK, WS, IN, OUT)
+  // if(!I2S.setDuplex()){
+  //   Serial.println("ERROR - could not set duplex");
+  //   while(true){
+  //     vTaskDelay(10); // Cannot continue
+  //   }
+  // }
+  // I2S.setAllPins(int sckPin, int fsPin, int sdPin, int outSdPin, int inSdPin);
+
+  // //I2SClass::I2SClass() a;
+
+  // if (!I2S.begin(I2S_PHILIPS_MODE, sampleRate, bitsPerSample)) {
+  //   Serial.println("Failed to initialize I2S!");
+  //   while(true){
+  //     vTaskDelay(10); // Cannot continue
+  //   }
+  // }
+  // buffer = (uint8_t*) malloc(I2S.getBufferSize() * (bitsPerSample / 8));
+  // if(buffer == NULL){
+  //   Serial.println("Failed to allocate buffer!");
+  //   while(true){
+  //     vTaskDelay(10); // Cannot continue
+  //   }
+  // }
+  // Serial.println("Setup done");
 
 }
 
 void loop() {
   //I2S.write(I2S.read()); // primitive implementation sample-by-sample
-
+  //Serial.println("asjgi");
   // Buffer based implementation
-  I2S.read(buffer, I2S.getBufferSize() * (bitsPerSample / 8));
-  I2S.write(buffer, I2S.getBufferSize() * (bitsPerSample / 8));
+  //I2S.read(buffer, I2S.getBufferSize() * (bitsPerSample / 8));
+  //I2S.write(buffer, I2S.getBufferSize() * (bitsPerSample / 8));
+  int sample = I2S.read();
+  Serial.println(sample);
 
   //optimistic_yield(1000); // yield if last yield occurred before <parameter> CPU clock cycles ago
 
